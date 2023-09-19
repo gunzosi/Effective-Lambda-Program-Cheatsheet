dd43bac8481323be35b5b5e9d24f7e36c39c2570
Dang Dai Duong <oceanwork2820@gmail.com>
2023-09-20 01:12:12 +0700

    Create biPre.java

---
+++ utilFunction/biPre.java
@@ -0,0 +1,20 @@
+import java.util.function.BiPredicate;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+
+public class biPre {
+    public static void main(String[] args) {
+        // BiPredicate example
+        BiPredicate<Integer, String> isLengthEqual = (num, str) -> str.length() == num;
+        System.out.println(isLengthEqual.test(5, "Hello")); // Output: true
+        System.out.println(isLengthEqual.test(5, "World")); // Output: false
+
+        // BiConsumer example
+        BiConsumer<String, Integer> printKeyValue = (key, value) -> System.out.println(key + " : " + value);
+        printKeyValue.accept("Age", 30); // Output: Age : 30
+
+        // BiFunction example
+        BiFunction<Integer, Integer, Integer> add = (a, b) -> a + b;
+        System.out.println(add.apply(5, 3)); // Output: 8
+    }
+}
